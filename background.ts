ad1e8c82c7d7b7102a5554262f3ddd58242eb927
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-05-23 15:45:10 +0800

    add new features

--- background.ts
+++ background.ts
@@ -7,13 +7,21 @@ import sanitizeHtml = require('sanitize-html')
 
 let toMarkdown = require('to-markdown')
 
-const SELECTION_TO_MARKDOWN = 'SELECTION_TO_MARKDOWN',
+const TAB_URL_TO_MARKDOWN = 'TAB_URL_TO_MARKDOWN',
+      TAB_URL_TO_HTML = 'TAB_URL_TO_HTML',
+      LINK_TO_MARKDOWN = 'LINK_TO_MARKDOWN',
+      LINK_TO_HTML = 'LINK_TO_HTML',
+      SELECTION_TO_MARKDOWN = 'SELECTION_TO_MARKDOWN',
       SELECTION_TO_MARKDOWN_WITHOUT_HTML = 'SELECTION_TO_MARKDOWN_WITHOUT_HTML',
       SELECTION_TO_HTML = 'SELECTION_TO_HTML',
       SELECTION_TO_HTML_LINK_ONLY = 'SELECTION_TO_HTML_LINK_ONLY',
       SELECTION_TO_PLAIN = 'SELECTION_TO_PLAIN',
+      IMAGE_TO_MARKDOWN = 'IMAGE_TO_MARKDOWN',
+      IMAGE_TO_HTML = 'IMAGE_TO_HTML',
       IMAGE_TO_DATA_URI_JPEG = 'IMAGE_TO_DATA_URI_JPEG',
-      IMAGE_TO_DATA_URI_PNG = 'IMAGE_TO_DATA_URI_PNG'
+      IMAGE_TO_DATA_URI_PNG = 'IMAGE_TO_DATA_URI_PNG',
+      AUDIO_TO_HTML = 'AUDIO_TO_HTML',
+      VIDEO_TO_HTML = 'VIDEO_TO_HTML'
 
 function setClipboard(text: string) : void {
   console.log(text)
@@ -41,6 +49,12 @@ function getDataURI(src: string, encoder: string = 'jpeg') : Promise<string> {
   })
 }
 
+function createMenus(...contexts: string[]) : (createPropertiesList: chrome.contextMenus.CreateProperties[]) => void {
+  return createPropertiesList =>
+    createPropertiesList.forEach(createProperties =>
+      chrome.contextMenus.create(Object.assign({}, createProperties, { contexts: contexts })))
+}
+
 const sendMessage = Promise.promisify<any, number, any>(
   (tabId: number, message: any, responseCallback?: (error: any, response: any) => void) =>
     chrome.tabs.sendMessage(tabId, message, response =>
@@ -48,91 +62,137 @@ const sendMessage = Promise.promisify<any, number, any>(
     )
 )
 
-chrome.contextMenus.create({
-  id: SELECTION_TO_MARKDOWN,
-  title: 'Selection to Markdown',
-  contexts: ['selection']
-})
+createMenus('page')([
+  {
+    id: TAB_URL_TO_MARKDOWN,
+    title: 'Tab to Markdown'
+  }, {
+    id: TAB_URL_TO_HTML,
+    title: 'Tab to HTML'
+  }
+])
 
-chrome.contextMenus.create({
-  id: SELECTION_TO_MARKDOWN_WITHOUT_HTML,
-  title: 'Selection to Markdown(without HTML)',
-  contexts: ['selection']
-})
+createMenus('link')([
+  {
+    id: LINK_TO_MARKDOWN,
+    title: 'Link to Markdown'
+  }, {
+    id: LINK_TO_HTML,
+    title: 'Link to HTML'
+  }
+])
 
-chrome.contextMenus.create({
-  id: 'separator1',
-  type: 'separator',
-  contexts: ['selection']
-})
+createMenus('selection')([
+  {
+    id: SELECTION_TO_MARKDOWN,
+    title: 'Selection to Markdown'
+  }, {
+    id: SELECTION_TO_MARKDOWN_WITHOUT_HTML,
+    title: 'Selection to Markdown(without HTML)'
+  }, {
+    id: 'separator1',
+    type: 'separator'
+  }, {
+    id: SELECTION_TO_HTML,
+    title: 'Selection to HTML'
+  }, {
+    id: SELECTION_TO_HTML_LINK_ONLY,
+    title: 'Selection to HTML(link only)'
+  }, {
+    id: 'separator2',
+    type: 'separator'
+  }, {
+    id: SELECTION_TO_PLAIN,
+    title: 'Selection to Plain'
+  }
+])
 
-chrome.contextMenus.create({
-  id: SELECTION_TO_HTML,
-  title: 'Selection to HTML',
-  contexts: ['selection']
-})
+createMenus('image')([
+  {
+    id: IMAGE_TO_MARKDOWN,
+    title: 'Image to Markdown'
+  }, {
+    id: IMAGE_TO_HTML,
+    title: 'Image to HTML'
+  }, {
+    id: IMAGE_TO_DATA_URI_JPEG,
+    title: 'Image to Data URI(JPEG)'
+  }, {
+    id: IMAGE_TO_DATA_URI_PNG,
+    title: 'Image to Data URI(PNG)'
+  }
+])
 
-chrome.contextMenus.create({
-  id: SELECTION_TO_HTML_LINK_ONLY,
-  title: 'Selection to HTML(link only)',
-  contexts: ['selection']
-})
+createMenus('audio')([
+  {
+    id: AUDIO_TO_HTML,
+    title: 'Audio to HTML'
+  }
+])
 
-chrome.contextMenus.create({
-  id: 'separator2',
-  type: 'separator',
-  contexts: ['selection']
-})
-
-chrome.contextMenus.create({
-  id: SELECTION_TO_PLAIN,
-  title: 'Selection to Plain',
-  contexts: ['selection']
-})
-
-chrome.contextMenus.create({
-  id: IMAGE_TO_DATA_URI_JPEG,
-  title: 'Image to Data URI(JPEG)',
-  contexts: ['image']
-})
-
-chrome.contextMenus.create({
-  id: IMAGE_TO_DATA_URI_PNG,
-  title: 'Image to Data URI(PNG)',
-  contexts: ['image']
-})
-
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  switch (info.menuItemId) {
-    case SELECTION_TO_MARKDOWN:
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  ({
+    TAB_URL_TO_MARKDOWN() {
+      setClipboard(`[${tab.title}](${tab.url})`)
+    },
+    TAB_URL_TO_HTML() {
+      setClipboard(`<a href="${tab.url}">${tab.title}</a>`)
+    },
+    LINK_TO_MARKDOWN({ linkUrl }) {
+      setClipboard(`[link](${linkUrl})`)
+    },
+    LINK_TO_HTML({ linkUrl }) {
+      setClipboard(`<a href="${linkUrl}">link</a>`)
+    },
+    async SELECTION_TO_MARKDOWN() {
       setClipboard(toMarkdown(await sendMessage(tab.id, { type: 'selection-html' })))
-      break
-    case SELECTION_TO_MARKDOWN_WITHOUT_HTML:
+    },
+    async SELECTION_TO_MARKDOWN_WITHOUT_HTML() {
       setClipboard(toMarkdown(await sendMessage(tab.id, { type: 'selection-html' })).replace(/<\/?[^>]+(>|$)/g, ''))
-      break
-    case SELECTION_TO_HTML:
+    },
+    async SELECTION_TO_HTML() {
       setClipboard(await sendMessage(tab.id, { type: 'selection-html' }))
-      break
-    case SELECTION_TO_HTML_LINK_ONLY:
+    },
+    async SELECTION_TO_HTML_LINK_ONLY() {
       setClipboard(sanitizeHtml(await sendMessage(tab.id, { type: 'selection-html' }), {
         allowedTags: ['a'],
         allowedAttributes: {
           'a': ['href']
         }
       }))
-      break
-    case SELECTION_TO_PLAIN:
+    },
+    async SELECTION_TO_PLAIN() {
       setClipboard(await sendMessage(tab.id, { type: 'selection-text'}))
-      break
-    case IMAGE_TO_DATA_URI_JPEG:
-      if (info.mediaType === 'image') {
-        setClipboard(await getDataURI(info.srcUrl, 'jpeg'))
+    },
+    IMAGE_TO_MARKDOWN({ mediaType, srcUrl }) {
+      if (mediaType === 'image') {
+        setClipboard(`![](${srcUrl})`)
       }
-    case IMAGE_TO_DATA_URI_PNG:
-      if (info.mediaType === 'image') {
-        setClipboard(await getDataURI(info.srcUrl, 'png'))
+    },
+    IMAGE_TO_HTML({ mediaType, srcUrl }) {
+      if (mediaType === 'image') {
+        setClipboard(`<img src="${srcUrl}" />`)
       }
-      break
-    default:
-  }
+    },
+    async IMAGE_TO_DATA_URI_JPEG({ mediaType, srcUrl }) {
+      if (mediaType === 'image') {
+        setClipboard(await getDataURI(srcUrl, 'jpeg'))
+      }
+    },
+    async IMAGE_TO_DATA_URI_PNG({ mediaType, srcUrl }) {
+      if (mediaType === 'image') {
+        setClipboard(await getDataURI(srcUrl, 'png'))
+      }
+    },
+    AUDIO_TO_HTML({ mediaType, srcUrl }) {
+      if (mediaType === 'audio') {
+        setClipboard(`<audio controls="controls" src="${srcUrl}"></audio>`)
+      }
+    },
+    VIDEO_TO_HTML({ mediaType, srcUrl }) {
+      if (mediaType === 'video') {
+        setClipboard(`<video controls="controls" src="${srcUrl}"></video>`)
+      }
+    }
+  })[info.menuItemId](info)
 })
