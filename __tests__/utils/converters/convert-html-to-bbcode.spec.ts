4583c7f4f8621e56ce9074a30e95380b0630410d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-22 22:11:30 +0800

    test: add cases

--- __tests__/utils/converters/convert-html-to-bbcode.spec.ts
+++ __tests__/utils/converters/convert-html-to-bbcode.spec.ts
@@ -1,7 +1,27 @@
 import { convertHTMLToBBCode } from '@converters/convert-html-to-bbcode.js'
 
-test('convertHtmlToBBCode', () => {
-  const result = convertHTMLToBBCode('<a href="/"><img src="hello.jpg" /></a>')
+describe('convertHtmlToBBCode', () => {
+  test('img', () => {
+    const html = '<img src="hello.jpg" />'
 
-  expect(result).toBe('[url=/][img]hello.jpg[/img][/url]')
+    const result = convertHTMLToBBCode(html)
+
+    expect(result).toBe('[img]hello.jpg[/img]')
+  })
+
+  test('a', () => {
+    const html = '<a href="/">Text</a>'
+
+    const result = convertHTMLToBBCode(html)
+
+    expect(result).toBe('[url=/]Text[/url]')
+  })
+
+  test('font-size', () => {
+    const html = `<span style="font-size: 30px">Large Text</span>`
+
+    const result = convertHTMLToBBCode(html)
+
+    expect(result).toBe('[size=30]Large Text[/size]')
+  })
 })
