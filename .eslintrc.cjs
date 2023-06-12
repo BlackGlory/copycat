aeac280f8b42d4102ca3c78237cf732536edd5a7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-06-12 10:09:28 +0000

    chore: add linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,24 @@ module.exports = {
 , parser: '@typescript-eslint/parser'
 , plugins: [
     '@typescript-eslint'
+  , 'react'
+  , 'react-hooks'
   ]
 , extends: [
     'eslint:recommended'
   , 'plugin:@typescript-eslint/recommended'
+  , 'plugin:react/recommended'
+  , 'plugin:react/jsx-runtime'
+  , 'plugin:react-hooks/recommended'
   ]
 , rules: {
     'no-async-promise-executor': 'off'
   , '@typescript-eslint/ban-ts-comment': 'off'
   , '@typescript-eslint/no-inferrable-types': 'off'
+  }
+, settings: {
+    react: {
+      'version': 'detect'
+    }
   }
 }
