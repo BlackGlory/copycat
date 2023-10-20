62b7d4abd314b50fd582473a21aec102e3a923c8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-10-20 18:20:48 +0800

    fix: cross platformer behaviors

--- src/converters/url/formatted/relative.ts
+++ src/converters/url/formatted/relative.ts
@@ -1,11 +1,11 @@
-import { relative } from 'path'
+import { posix } from 'path'
 
 export function convertUrlToRelativeURL(absoluteUrl: string, baseUrl: string): string {
   try {
     const absoluteUrlObj = new URL(absoluteUrl)
     const baseUrlObj = new URL(baseUrl)
     if (absoluteUrlObj.origin === baseUrlObj.origin) {
-      return relative(baseUrlObj.pathname, absoluteUrlObj.pathname)
+      return posix.relative(baseUrlObj.pathname, absoluteUrlObj.pathname)
     } else {
       return absoluteUrl
     }
