b28d673ba1abfbff2ea7b6a28a31689fa4c2c0de
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-11-07 20:09:35 +0800

    chore: remove unnecessary commas

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -35,7 +35,7 @@ module.exports = {
       }
     , {
         test: /\.css$/i
-      , use: ['style-loader', 'css-loader', 'postcss-loader'],
+      , use: ['style-loader', 'css-loader', 'postcss-loader']
       }
     ]
   }
