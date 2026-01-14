5f879db3a6050e8b2885075e55a457e01d042fbf
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-01-14 17:46:39 +0800

    refactor: simplify

--- src/utils/convert-html-to-bbcode.ts
+++ src/utils/convert-html-to-bbcode.ts
@@ -1,7 +1,8 @@
-import html2bbcode from 'html2bbcode'
+import { HTML2BBCode } from 'html2bbcode'
+import { lazy } from '@blackglory/prelude'
 
-const { HTML2BBCode } = html2bbcode
+const getHTML2BBCode = lazy(() => new HTML2BBCode())
 
 export function convertHTMLToBBCode(html: string): string {
-  return (new HTML2BBCode()).feed(html).toString()
+  return getHTML2BBCode().feed(html).toString()
 }
